fe4c541e14aa3e90d356143ae13f7d9d0e1129f3
Tyler F <90405742+s11441243@users.noreply.github.com>
2021-10-28 12:06:17 -0400

    [7B] v0.1

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,11 @@
-# Variable Practcie, Ryan Kelley, 10/28/21 11:39am, version 0.0
+# Variable Practcie, Tyler Fann, 10/28/21 12:05pm, version 0.1
+
+# Variable Types -- Most Common
+
+my_float = 1.23455
+my_int = -2135125638
+my_string = "I like football."
+
+print(my_float)
+print(my_int)
+print(my_string)
